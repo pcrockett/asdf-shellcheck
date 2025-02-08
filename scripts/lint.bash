fea341d1dd1e420bd0d027f6d7170468242427e1
Phil Crockett <contact@philcrockett.com>
2025-02-08 09:54:54 +0100

    chore(ci): create a CI process (#2)
    Copy / paste / modified from https://github.com/pcrockett/asdf-bashly

---
+++ scripts/lint.bash
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+shellcheck --shell=bash --external-sources \
+	bin/* --source-path=template/lib/ \
+	scripts/*
+
+shfmt --language-dialect bash --diff \
+	./**/*
