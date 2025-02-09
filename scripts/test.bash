51f182e032db8cca6b4510af584f42bf285eec62
Phil Crockett <contact@philcrockett.com>
2025-02-09 07:57:30 +0100

    get CI working by pinning test to shellcheck 0.9.0

--- scripts/test.bash
+++ scripts/test.bash
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
+SHELLCHECK_VERSION="0.9.0" # temp workaround for macOS. TODO: change to latest
+
 step() {
 	echo "--> ${*}"
 }
@@ -9,14 +11,14 @@ main() {
 	step "installing shellcheck plugin..."
 	asdf plugin add shellcheck ./.git
 
-	step "installing shellcheck latest..."
-	asdf install shellcheck latest
-	asdf local shellcheck latest
+	step "installing shellcheck ${SHELLCHECK_VERSION}..."
+	asdf install shellcheck "${SHELLCHECK_VERSION}"
+	asdf local shellcheck "${SHELLCHECK_VERSION}"
 
 	step "testing shellcheck path..."
 	shellcheck_path="$(asdf which shellcheck)"
-	echo "${shellcheck_path}" \
-		| grep --fixed-strings --color=always "asdf/installs/shellcheck"
+	echo "${shellcheck_path}" |
+		grep --fixed-strings --color=always "asdf/installs/shellcheck"
 
 	test_cmd=(
 		"${shellcheck_path}"
